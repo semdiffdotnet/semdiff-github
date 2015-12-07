c17422e6391182efb95eb3892767e00ccfea2659
Cody Ray Hoeft <crhoeft@gmail.com>
2015-12-07 13:39:19 -0800

    Stash these changes in case I want them latter

---
+++ SemDiff-GitHub/TestAnalyzer/TestAnalyzer/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+
+public interface Class1
+{
+    void OnSyntaxTree(SyntaxTreeAnalysisContext context);
+    void OnSyntaxNode(SyntaxNodeAnalysisContext context);
+    void OnSymbol(SymbolAnalysisContext context);
+    void OnSemanticModel(SemanticModelAnalysisContext context);
+    void OnCompilation(CompilationAnalysisContext context);
+    void OnCodeBlock(CodeBlockAnalysisContext context);
+    void OnCompilationStart(CompilationStartAnalysisContext context);
+    void OnCodeBlockStart(CodeBlockStartAnalysisContext<SyntaxKind> context);
+}
